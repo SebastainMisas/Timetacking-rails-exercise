4ce72b950c81b1c1a948a5dc3a24209a7fc8172d
Sebastain Misas <juanmisas2@gmail.com>
2015-11-23 10:45:59 -0500

    added update feature to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,14 +4,6 @@ class ProjectsController < ApplicationController
 		@projects_list = Project.order(created_at: :desc) 
 		render "index"
 	end
-	def show
-		id = params[:id]
-		@proj = Project.find_by(id: id)
-		
-		render "show"
-		
-
-	end
 	def new
 		@project = Project.new
 		render "new"
@@ -23,9 +15,33 @@ class ProjectsController < ApplicationController
 		if @project.save 
 			redirect_to("/projects")
 		else
-			redirect_to("/404")
+			redirect_to("/new")
 		end
 	end
+	def edit
+		id = params[:id]
+		@project = Project.find_by(id: id)
+		if @project == nil
+			redirect_to("/404")
+		else
+			render "edit"
+		end
+
+	end
+	def update
+		id = params[:id]
+		@project = Project.find_by(id: id)
+		if @project == nil
+			redirect_to("/404")
+		else
+			if @project.update(project_params)
+				redirect_to "/projects"
+			else
+				render("edit")
+			end
+		end
+	end
+
 # private means none of this information is not accessable outside of the controller
 	private
 	def project_params
